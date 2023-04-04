88f221562afa419b521eed028cb74b40c51b3f75
jimmy0412 <k56ipu5050@gmail.com>
2023-04-04 15:22:43 +0800

    basic about

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, session, render_template, send_file, flash 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
 	return "Hello World!"
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
